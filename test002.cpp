25ba2c9e961159a0ddadc5e7074dafb1c7f9eaa9
TGLockHart <113041823+TGLockHart@users.noreply.github.com>
2022-09-07 22:49:37 +0700

    Add files via upload

---
+++ test002.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch ,mychar;
+    int Count=0;
+    cin >>mychar;
+    for(mychar;ch!=mychar;cin>>ch){
+        Count++;
+    }cout << Count ;
+  return 0;
+}
